ae0a5d6b203d2e8cb880130afa2bbf4931ce3046
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-07-27 23:27:05 +0200

    Added basic skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const paellaImaegeUrl: string =
+    "https://images.hola.com/imagenes/cocina/recetas/20200917175530/paella-valenciana-clasica/0-866-670/paella-age-m.jpg";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <>
+      <h1>Hallo</h1>
+      <section className="main-content">
+        <img
+          src={paellaImaegeUrl}
+          alt="tremenda paella"
+          className="main-content__image"
+        />
+        <p className="main-content__text">
+          Ay qué rica la paella que aparece en la pantalla. ¿A quién no le va a
+          gustar una paella valenciana del siglo XX?¿A quién no le va a gustar?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p className="main-content__text main-content__text--highlighted">
+          Os invito a todos a que me invitéis a una paellita bien rica y
+          participéis mucho en esta clase.
+        </p>
+        <p className="main-content__text">
+          Ánimos con Flux, mis bebesitos y bebesitas fiu fiu. Gerard, si lees
+          esto eres mi{" "}
+          <span className="main-context__text--love">pionono de vitrina</span>.
+        </p>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button className="button">Get paella for free</button>;
+};
+
+export default Button;
