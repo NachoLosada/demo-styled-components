df237b9d97ddc270a5f1d5aeb9665d1ef445aa54
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-07-27 23:30:42 +0200

    Added text container

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-function App() {
+import Button from "./components/Button/Button";
+
+const App = (): JSX.Element => {
   const paellaImaegeUrl: string =
     "https://images.hola.com/imagenes/cocina/recetas/20200917175530/paella-valenciana-clasica/0-866-670/paella-age-m.jpg";
 
@@ -11,22 +13,27 @@ function App() {
           alt="tremenda paella"
           className="main-content__image"
         />
-        <p className="main-content__text">
-          Ay qué rica la paella que aparece en la pantalla. ¿A quién no le va a
-          gustar una paella valenciana del siglo XX?¿A quién no le va a gustar?
-        </p>
-        <p className="main-content__text main-content__text--highlighted">
-          Os invito a todos a que me invitéis a una paellita bien rica y
-          participéis mucho en esta clase.
-        </p>
-        <p className="main-content__text">
-          Ánimos con Flux, mis bebesitos y bebesitas fiu fiu. Gerard, si lees
-          esto eres mi{" "}
-          <span className="main-context__text--love">pionono de vitrina</span>.
-        </p>
+        <div className="main-content__text-container">
+          <p className="main-content__text">
+            Ay qué rica la paella que aparece en la pantalla. ¿A quién no le va
+            a gustar una paella valenciana del siglo XX?¿A quién no le va a
+            gustar?
+          </p>
+          <p className="main-content__text main-content__text--highlighted">
+            Os invito a todos a que me invitéis a una paellita bien rica y
+            participéis mucho en esta clase.
+          </p>
+          <p className="main-content__text">
+            Ánimos con Flux, mis bebesitos y bebesitas fiu fiu. Gerard, si lees
+            esto eres mi{" "}
+            <span className="main-context__text--love">pionono de vitrina</span>
+            .
+          </p>
+        </div>
+        <Button />
       </section>
     </>
   );
-}
+};
 
 export default App;
